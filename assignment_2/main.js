82bc2d9b17d6faf9bf722a4a60874d2232a10240
kngo1337 <60367871+kngo1337@users.noreply.github.com>
2020-04-02 18:33:26 -0400

    update

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -5,9 +5,28 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for (var i=1;i<=5;i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic'+i+'.jpg' || 'images/pic'+i+'.jpeg' );
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", onClickImage);    
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+function onClickImage(newImage){
+    const image = newImage.target.getAttribute("src");
+    displayedImage.setAttribute("src", image);
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+    const darkNLight = btn.getAttribute("class")
+    if (darkNLight === "light") {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0, 0 , 0, 0)";
+      } else {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0, 0 , 0, 0.5)";
+      }
+}
