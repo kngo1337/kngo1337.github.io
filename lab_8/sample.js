91f9a50652a6ba652bd9dbeb00256febf8b153bf
kngo1337 <60367871+kngo1337@users.noreply.github.com>
2020-04-11 01:03:08 -0400

    complete

--- lab_8/sample.js
+++ lab_8/sample.js
@@ -2,12 +2,19 @@ fetch(' https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json') // repl
     .then((response) => {
         console.log("response information", response);
         return response;
-})
+    })
     .then((data) => data.json()) // this is an "implicit return" - we're returning the results of the Fetch request to the next step.
     .then((data) => { // this is an explicit return. If I want my information to go further, I'll need to use the "return" keyword before the brackets close
         console.log(data);
+        console.log("entries", data.length);
+        const clearEmpty = data.filter((f) => f.geocoded_column_1);
+        const refined = clearEmpty.map((m)=>({
+            category: m.category,
+            name: m.name,
+            latLong: m.geocoded_column_1.coordinates,
+        })); 
         return data; // <- this will pass the data to the next "then" statement when I'm ready.
-        })
+    })
 
     .then((data) =>{
         return data.reduce((result,current)=>{
@@ -21,7 +28,7 @@ fetch(' https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json') // repl
 
     .then((data) =>{
         console.log('new data', data);
-        const reformattedData  = Object.defineProperties(data).map((m,i)=>{
+        const reformattedData  = Object.entries(data).map((m,i)=>{
             console.log(m);
             return{
                 y: m[1].length,
@@ -32,7 +39,45 @@ fetch(' https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json') // repl
         return reformattedData;
     })
 
-    .then((results)=>{
-        console.log(results);
-        const chart = new CanvasJS.Chart("chartContainer",)
-    })
+    .then((data)=>{
+        console.log(data);
+        const chart = new CanvasJS.Chart("chartContainer",{
+            animationEnabled: true,
+	        theme: "light2", // "light1", "light2", "dark1", "dark2"
+	        title:{
+		        text: "Let's Find Somewhere to Eat"
+            },
+            axisX:{
+                labelFontsize: 8,
+                margin: 10,
+                interval: 1,
+            },
+
+	        axisY: {
+                
+		        scaleBreaks:{
+                    customBreaks: [
+                        {
+                            startValue: 20,
+                            endValue: 35,
+                        },
+                        {
+                            startValue:60,
+                            endValue:120 , 
+                        },
+                        {
+                            startValue:160,
+                            endValue: 220,
+                        },
+                    ],
+                },  
+	        },
+	        data: [
+                {        
+		            type: "bar",
+		            dataPoints: data,
+                },
+            ],
+        });
+        chart.render();
+    });
